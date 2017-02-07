834f576015c88cdc767f3757d9503704f940c5e8
hharris3 <houston_harris@intuit.com>
2017-02-07 15:35:00 -0600

    first commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,17 +2,50 @@ import React from 'react'
 import { IndexLink, Link } from 'react-router'
 import './Header.scss'
 
-export const Header = () => (
-  <div>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName='route--active'>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='route--active'>
-      Counter
-    </Link>
-  </div>
-)
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleScroll = this.handleScroll.bind(this);
+    this.state = {
+      scrollPos: 0
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(e) {
+    // get scroll position
+    let scrollTop = e.srcElement.body.scrollTop;
+    // just display for now
+    this.setState({
+      scrollPos: scrollTop
+    });
+  }
+
+  render() {
+    let headerClass = 'header';
+    if (this.state.scrollPos > 56) {
+      headerClass += ' dark';
+    }
+    return (
+      <div className={headerClass}>
+        <IndexLink to='/' activeClassName='route--active'>
+          Home
+        </IndexLink>
+        <Link to='/counter' activeClassName='route--active'>
+          Counter
+        </Link>
+      </div>
+    );
+  }
+
+}
 
 export default Header
--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,15 +1,52 @@
 import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
-import './HomeView.scss'
+import { Parallax, Background } from 'react-parallax';
+import DuckImage from '../assets/Duck.jpg';
+import AlaskaImage from '../assets/alaska.jpg';
+import FamilyImage from '../assets/family.jpg';
+import './HomeView.scss';
 
-export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img
-      alt='This is a duck, because Redux!'
-      className='duck'
-      src={DuckImage} />
-  </div>
-)
+class HomeView extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className='home-top'>
+
+        <Parallax strength={-200} blur={2}>
+          <Background>
+            <img src={AlaskaImage}/>
+          </Background>
+          <div className='profile-container'>
+            <img
+              alt='Harris Family'
+              className='profile-image'
+              src={FamilyImage} />
+            <h2>Harris Family Launch Pad</h2>
+          </div>
+        </Parallax>
+
+        <div className='container'>
+          <h4>Welcome!</h4>
+          <img
+            alt='This is a duck, because Redux!'
+            className='duck'
+            src={DuckImage} />
+
+          <p>
+            For Literature, the students are writing a compare/contrast essay for A Single Shard (Korea) and Matilda Bone (England).  I have attached a document with new instructions for that paper, due February 23.  Also, they should be reading and listening to Good Masters! Sweet Ladies!  The Plano Library system has both the book and audio.  The assignment for this book is not to write an essay, but rather to recite a memorized piece which can be either one of the monologues or dialogues in the book or an alternate piece of your (the teacher's) choosing.  However, the recitation should be about 3 minutes long.  The recitation will be on March 16, the week after Spring Break.
+          </p>
+
+        </div>
+
+      </div>
+    );
+  }
+
+}
 
 export default HomeView
