9acfce7196dcaa5af628ba255e42920e46df3cd3
houston88 <houstyharris@gmail.com>
2020-03-22 00:24:20 +0000

    Moving site over to https

--- src/api/twitterData.js
+++ src/api/twitterData.js
@@ -1,16 +1,16 @@
 import { getJSON } from './http'
 
 export const getHappiestStates = () => {
-  let url = 'http://housty.io/api/happiestStates'
+  let url = 'https://housty.io/api/happiestStates'
   return getJSON(url)
 }
 
 export const getHappiestStateDetails = (id) => {
-  let url = 'http://housty.io/api/happiestStates/' + id
+  let url = 'https://housty.io/api/happiestStates/' + id
   return getJSON(url)
 }
 
 export const getHistoricalHapiness = () => {
-  let url = 'http://housty.io/api/happiestTimes'
+  let url = 'https://housty.io/api/happiestTimes'
   return getJSON(url)
 }
