1897f8b74f0e4c6b816bf9ec5219c7b3f157de82
houston88 <houstyharris@gmail.com>
2020-07-18 14:23:51 -0500

    Lesson link fixes, added to nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,68 +1,62 @@
-import React, { PropTypes } from 'react'
-import { IndexLink, Link } from 'react-router'
-import './Header.scss'
+import React, { PropTypes } from "react";
+import { IndexLink, Link } from "react-router";
+import "./Header.scss";
 
 class Header extends React.Component {
-
-  constructor (props) {
-    super(props)
-    this.handleScroll = this.handleScroll.bind(this)
+  constructor(props) {
+    super(props);
+    this.handleScroll = this.handleScroll.bind(this);
     this.state = {
-      scrollPos: 0
-    }
+      scrollPos: 0,
+    };
   }
 
-  componentDidMount () {
-    document.addEventListener('scroll', this.handleScroll)
+  componentDidMount() {
+    document.addEventListener("scroll", this.handleScroll);
   }
 
-  componentWillUnmount () {
-    document.removeEventListener('scroll', this.handleScroll)
+  componentWillUnmount() {
+    document.removeEventListener("scroll", this.handleScroll);
   }
 
-  handleScroll (e) {
+  handleScroll(e) {
     // get scroll position
-    let scrollTop = document.documentElement.scrollTop
+    let scrollTop = document.documentElement.scrollTop;
     // set to state... need to debounce
     this.setState({
-      scrollPos: scrollTop
-    })
+      scrollPos: scrollTop,
+    });
   }
 
-  render () {
-    const { location } = this.props
-    let headerClass = 'header'
+  render() {
+    const { location } = this.props;
+    let headerClass = "header";
     if (this.state.scrollPos > 56) {
-      headerClass += ' dark'
+      headerClass += " dark";
     }
     return (
       <div className={headerClass}>
-        { location.pathname.indexOf('/debis-resources') === 0
-          ? <Link to='/debis-resources' activeClassName='route--active'>
-            Debi's Resources
+        <div>
+          <IndexLink to="/" activeClassName="route--active">
+            Home
+          </IndexLink>
+          <Link to="/twitter-data" activeClassName="route--active">
+            Twitter Data
           </Link>
-          : <div>
-            <IndexLink to='/' activeClassName='route--active'>
-              Home
-            </IndexLink>
-            <Link to='/twitter-data' activeClassName='route--active'>
-              Twitter Data
-            </Link>
-            <Link to='/resume' activeClassName='route--active'>
-              Resume
-            </Link>
-            {/* <Link to='/counter' activeClassName='route--active'>
-              Counter
-            </Link> */}
-          </div>}
+          <Link to="/resume" activeClassName="route--active">
+            Resume
+          </Link>
+          <Link to="/debis-resources" activeClassName="route--active">
+            Debi
+          </Link>
+        </div>
       </div>
-    )
+    );
   }
-
 }
 
 Header.propTypes = {
-  location: PropTypes.object
-}
+  location: PropTypes.object,
+};
 
-export default Header
+export default Header;
